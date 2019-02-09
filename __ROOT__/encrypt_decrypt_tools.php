cce95fc7a2177ea045527a485c6da859d4dfcf19
mak <makindosx@gmail.com>
2019-02-09 17:45:14 -0500

    Add files via upload

---
+++ __ROOT__/encrypt_decrypt_tools.php
@@ -0,0 +1,91 @@
+<?php
+
+/*
+ * Copyright (c) 2019 Barchampas Gerasimos <makindosx@gmail.com>
+ * Tenebris is an encryption and decryption program to reach secure conversations.
+ *
+ * Tenebris is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ *
+ * Tenebris is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License, version 3,
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>
+ *
+ */
+
+
+class ENCRYPT_DECRYPT_AES_256_CBC_TOOLS
+{
+
+ public $ENCRYPT_DECRYPT_AES_TYPE = array();
+
+ public function __construct()
+ {
+
+  $this->ENCRYPT_DECRYPT_AES_TYPE[0] = "AES-256-CBC";
+  
+  }
+
+
+} 
+
+
+
+
+ class SECURE_INPUT_DATA
+    { 
+    
+      public function SECURE_DATA_ENTER($data)
+        {
+        $data = htmlspecialchars($data);
+        $data = htmlentities($data);
+        $data = trim($data);
+        $data = stripslashes($data);  
+        $data = stripcslashes ($data);
+          if ($data == true)
+           {
+          return ($data);
+            }          
+          else
+            {
+            return (!$data); //.trigger_error("Data was not be send safe");
+             }
+         } 
+    
+      } // end fo class SECURE_INPUT_DATA
+      
+      
+  class SECURE_INPUT_DATA_AVAILABLE extends SECURE_INPUT_DATA
+    { 
+    
+      public function SECURE_DATA_ENTER($data)
+        {
+        $data = htmlspecialchars($data);
+        $data = htmlentities($data);
+        $data = trim($data);
+        $data = stripslashes($data);  
+        $data = stripcslashes ($data);
+          if ($data == true)
+           {
+          return ($data);
+            }          
+          else
+            {
+            return (!$data); //.trigger_error("Data was not be send safe");
+             }
+         } 
+    
+      } // end fo class SECURE_INPUT_DATA_AVAILABLE
+
+
+
+
+
+?>
