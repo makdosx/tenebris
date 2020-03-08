9c2d68794e79ab5e32528e3638ed8fea678d4e85
mak <makindosx@gmail.com>
2020-03-09 01:00:06 +0200

    Create tenebris.sql

---
+++ sql/tenebris.sql
@@ -0,0 +1,71 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1ubuntu0.1
+-- http://www.phpmyadmin.net
+--
+-- Host: localhost
+-- Generation Time: Mar 09, 2020 at 12:58 AM
+-- Server version: 5.5.62-0ubuntu0.14.04.1
+-- PHP Version: 5.5.9-1ubuntu4.29
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `tenebris`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `backup_login`
+--
+
+CREATE TABLE IF NOT EXISTS `backup_login` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `instant` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user` varchar(24) NOT NULL,
+  `pass` varchar(32) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `login`
+--
+
+CREATE TABLE IF NOT EXISTS `login` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `instant` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `user` varchar(24) NOT NULL,
+  `pass` varchar(32) NOT NULL,
+  `aes_type` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=6 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `_keys`
+--
+
+CREATE TABLE IF NOT EXISTS `_keys` (
+  `id` int(10) NOT NULL AUTO_INCREMENT,
+  `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `public_key` varchar(32) NOT NULL,
+  `private_key` varchar(16) NOT NULL,
+  `used_key` varchar(64) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `public_key` (`public_key`),
+  UNIQUE KEY `private_key` (`private_key`)
+) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=2417 ;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
