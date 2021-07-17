24f3d60caaf40e12dea4ad0fa1fde5c87f72eaf6
mak <makindosx@gmail.com>
2021-07-17 01:55:22 -0400

    Add files via upload

--- sql/tenebris.sql
+++ sql/tenebris.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10deb1ubuntu0.1
--- http://www.phpmyadmin.net
+-- version 4.6.6deb5ubuntu0.5
+-- https://www.phpmyadmin.net/
 --
--- Host: localhost
--- Generation Time: Mar 09, 2020 at 12:58 AM
--- Server version: 5.5.62-0ubuntu0.14.04.1
--- PHP Version: 5.5.9-1ubuntu4.29
+-- Host: localhost:3306
+-- Generation Time: Jul 17, 2021 at 01:37 AM
+-- Server version: 5.7.32-0ubuntu0.18.04.1
+-- PHP Version: 7.2.24-0ubuntu0.18.04.7
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,7 +14,7 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
 -- Database: `tenebris`
@@ -23,31 +23,17 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
--- Table structure for table `backup_login`
---
-
-CREATE TABLE IF NOT EXISTS `backup_login` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
-  `instant` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `user` varchar(24) NOT NULL,
-  `pass` varchar(32) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
-
--- --------------------------------------------------------
-
---
 -- Table structure for table `login`
 --
 
-CREATE TABLE IF NOT EXISTS `login` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `login` (
+  `id` int(10) NOT NULL,
   `instant` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `user` varchar(24) NOT NULL,
   `pass` varchar(32) NOT NULL,
-  `aes_type` varchar(64) NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+  `rev_pass` varchar(32) NOT NULL,
+  `aes_type` varchar(64) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -55,17 +41,46 @@ CREATE TABLE IF NOT EXISTS `login` (
 -- Table structure for table `_keys`
 --
 
-CREATE TABLE IF NOT EXISTS `_keys` (
-  `id` int(10) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `_keys` (
+  `id` int(10) NOT NULL,
   `created` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `public_key` varchar(32) NOT NULL,
   `private_key` varchar(16) NOT NULL,
-  `used_key` varchar(64) NOT NULL,
-  PRIMARY KEY (`id`),
-  UNIQUE KEY `public_key` (`public_key`),
-  UNIQUE KEY `private_key` (`private_key`)
-) ENGINE=InnoDB  DEFAULT CHARSET=latin1 AUTO_INCREMENT=1;
+  `used_key` varchar(64) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `login`
+--
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Indexes for table `_keys`
+--
+ALTER TABLE `_keys`
+  ADD PRIMARY KEY (`id`),
+  ADD UNIQUE KEY `public_key` (`public_key`),
+  ADD UNIQUE KEY `private_key` (`private_key`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `login`
+--
+ALTER TABLE `login`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
+--
+-- AUTO_INCREMENT for table `_keys`
+--
+ALTER TABLE `_keys`
+  MODIFY `id` int(10) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=25;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
